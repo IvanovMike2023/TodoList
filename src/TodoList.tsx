d5e3c15eb3ef37bbf49a6e63b1895bc799cc8043
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-03-28 14:18:25 +0300

    lesson 1

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,27 +1,40 @@
 import React, {FC} from "react";
 
-export type PropsTasks = {
-    id: number,
+ type PropsTasks = {
+    taskId: number,
     title: string,
     isDone: boolean
 }
-type PropsType = {
-    title: string,
-    tasks: PropsTasks[]
+ type PropsStudents = {
 }
-const TodoList:FC<PropsType>=(props) => {
+ export interface PropsData {
+    title: string,
+    tasks: PropsTasks[],
+     students: PropsStudents[]
+
+ }
+
+type PropsType = {
+    data: PropsData
+
+}
+const TodoList: FC<PropsType> = (props) => {
 //const TodoList=(props:PropsType)=>{
+    //const tasks=props.data.tasks
+
+    const data=props.data
+    console.log(data)
     return (
         <div className="todolist">
-            <h3>{props.title}</h3>
+            <h3>{data.title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+                <li><input type="checkbox" checked={data.tasks[0].isDone}/> <span>{data.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={data.tasks[1].isDone}/> <span>{data.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={data.tasks[2].isDone}/> <span>{data.tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
